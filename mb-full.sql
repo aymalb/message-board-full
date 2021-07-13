543ed454278a182067cc1d7b1fa1f4107f07b00f
aymalb <aymetdinov@gmail.com>
2021-07-13 09:11:16 +0300

    Added registration and login functionality

--- mb-full.sql
+++ mb-full.sql
@@ -26,8 +26,9 @@ CREATE TABLE `posts` (
   `id` int NOT NULL AUTO_INCREMENT,
   `posttext` longtext,
   `postdate` datetime DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,8 +37,32 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (44,'hi','2021-07-12 12:59:16'),(45,'hi','2021-07-12 12:59:18'),(46,'hi','2021-07-12 12:59:19');
+INSERT INTO `posts` VALUES (59,'Hello','2021-07-13 06:03:30','Al'),(60,'Hi','2021-07-13 06:04:03','Pal'),(61,'Hey','2021-07-13 06:04:20','Sal');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `name` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('Al','$2y$10$OC1PoE8fEvbs4G6ALF.Ev.FKxqxrsm.tRZWe6zcKXtmHkWEPvGMSe'),('Pal','$2y$10$q4sbD.PSVRjrubMRRHEtgu8H2U2Olp4wRlkYaWmj/bE3/cs7EaMtm'),('Sal','$2y$10$CI0bHdEyS2cNKIw2bThzPOctdS5W4QRX8bDiR9VHXUBCcu4MGutmS');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +74,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-12 13:09:24
+-- Dump completed on 2021-07-13  6:09:37
--- public/index.php
+++ public/index.php
@@ -5,63 +5,62 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-login.css">
     <title>Message Board</title>
 </head>
 <body>
-
+<header>
 <h1><a href="index.php">Message board</a></h1>
+</header>
 
-<section id="posts">
+<main>
 
 <?php
 
+include __DIR__ . '/../includes/functions.php';
+
+if (isset($_POST['logout'])) {
+    setcookie('username');
+    setcookie('password');
+    header('location:index.php');
+}
 
 
-try {
-
-    if (isset($_POST['posttext'])) {
-        $pdo = new PDO('mysql:host=localhost;dbname=mb; charset=utf8', 'homestead', 'secret');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $sql = 'INSERT INTO `posts` SET
-                `posttext` = :posttext,
-                `postdate` = NOW()';
-
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':posttext', $_POST['posttext']);
-        $stmt->execute();
-
-        header('location:index.php');
+if (isset($_POST['reg-log'])) {
+    switch($_POST['reg-log']) {
+        case 'reg':
+            if (isset($_POST['user']['name']) && $_POST['user']['password'] != '' && count(findUser($_POST['user']['name'])) < 1) {
+                register($_POST['user']['name'], $_POST['user']['password']);
+                echo 'Registration successful!';
+                unset($_POST);
+            } else {
+                echo 'Could not register: input missing or username unavailable or invalid';
+                unset($_POST);
+            }
+            break;
+        
+        case 'log':
+            if (isset($_POST['user']['name']) && isset($_POST['user']['password']) &&
+            count(findUser($_POST['user']['name'])) == 1 && login($_POST['user']['name'], $_POST['user']['password'])) {
+                setcookie('username', $_POST['user']['name'], time() + 3600 * 24 * 365);
+                setcookie('password', findUser($_POST['user']['name'])[0]['password'], time() + 3600 * 24 * 365);
+                header('location:index.php');
+            } else {
+                echo 'Could not log in';
+            }
+            break;
     }
+}
 
-    $pdo = new PDO('mysql:host=localhost;dbname=mb; charset=utf8', 'homestead', 'secret');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sql = 'SELECT * FROM `posts`';
-
-    $posts = $pdo->query($sql)->fetchAll();
-
-    foreach($posts as $post) {
-        echo '<article class="post"><div class="post-text">' . $post['posttext'] . '</div>' .
-        '<div class="post-date">Posted on ' . $post['postdate'] . '</div>' . '</article>';
-    }
-
-} catch (PDOException $e) {
-    echo 'An error has occured: ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine();
+if (isset($_COOKIE['username']) && isset($_COOKIE['password'])) {
+    include __DIR__ . '/../includes/main.html.php';
+} else {
+    include __DIR__ . '/../includes/login.html.php';
 }
 
 ?>
+</main>
 
-</section>
-
-<form action="" method="post">
-
-<label for="posttext" class="form-el">Enter your post here:</label>
-<textarea name="posttext" id="posttext" class="form-el" cols="40" rows="5"></textarea>
-
-<input type="submit" value="Submit" class="form-el">
-
-</form>
 
 </body>
 </html>
